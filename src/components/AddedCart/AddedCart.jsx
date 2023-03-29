4babe1e66fe409416780241b7a69da5be5341e69
kawsar hasan joy <kawsarhasanjoy342@gmail.com>
2023-03-29 16:36:13 +0600

    modiul 3 update

--- src/components/AddedCart/AddedCart.jsx
+++ src/components/AddedCart/AddedCart.jsx
@@ -8,9 +8,16 @@ const AddedCart = (props) => {
     
     let Total = 0;
     let TotalShipping = 0;
+    let quentiy = 0;
     for (const single of getsingle) {
-         Total = Total + single.price;
+        if (single.quantity === 0) {
+            single.quantity = 1;
+        }
+        console.log(single)
+         Total = Total + single.price * single.quantity;
          TotalShipping = TotalShipping + single.shipping
+         quentiy = quentiy + single.quantity ;
+
 
     }
     const tax = Total*7/100;
@@ -20,7 +27,7 @@ const AddedCart = (props) => {
     return (
         <div className='Product-summary'>
             <h3>Order Product </h3>
-            <p>Selected item : {props.cart.length}</p>
+            <p>Selected item : {quentiy}</p>
             <p>Total Price: ${Total}</p>
             <p>Shipping Charge:${TotalShipping} </p>
             <p>Tax : ${tax}</p>
